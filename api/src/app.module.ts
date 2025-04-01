87874cd0e983470988639f5eb8f57dd98032c305
Ethan Frot <contact@ethanfrot.com>
2025-04-01 21:29:19 +0200

    🔧 Update docker config

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 import { MessagesModule } from './messages/messages.module';
 import { Gateway } from './gateway';
 
@@ -15,12 +15,12 @@ import { Gateway } from './gateway';
       port: 5432,
       username: 'postgres',
       password: 'postgres',
-      database: 'test',
+      database: 'chatapp',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, // À désactiver en production
     }),
-    UsersModule,
     AuthModule,
+    UsersModule,
     MessagesModule,
   ],
   providers: [Gateway],
